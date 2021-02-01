244788fea6baab0e5c94826163305631e73cf679
AhmedHome <ahmedmohamedaneng@gmail.com>
2021-02-01 14:33:33 +0200

    HoverTool added

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from motion_detector import df
 from bokeh.io import output_file, show
 from bokeh.plotting import figure
+from bokeh.models import HoverTool,ColumnDataSource
 
+
+
+# Optional perparing data for the hover_tool
+df["Start_str"] = df['Start'].dt.strftime("%D %H:%M:%S")
+df["End_str"] = df['End'].dt.strftime("%D %H:%M:%S")
+# Optional perparing ColumnDataSource 
+source = ColumnDataSource(df)
 
 
 #1# prepare some data
@@ -17,9 +25,17 @@ f.title.text_font_style = "italic"
 f.yaxis.minor_tick_line_color = None # to remove the scale line from yaxis
 
 
+# Optional adding a hover tool 
+hover = HoverTool(tooltips= [("Start", "@Start_str"),("End","@End_str")])
+f.add_tools(hover)
 
 #4# create a quad plot
-f.quad(left=df["Start"],right=df["End"],bottom=0,top=1,color="green")
+#f.quad(left=df["Start"],right=df["End"],bottom=0,top=1,color="green")
+
+
+# Start and End the column date names
+f.quad(source = source, left="Start",right="End",bottom=0,top=1,color="green") # use this if u use ColumnarDataSource
+
 
 #5# prepare the output_file
 output_file('motion_graph.html')
